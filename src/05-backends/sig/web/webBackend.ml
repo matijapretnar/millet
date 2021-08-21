a98cb0bb2a4a7bb73aa9dafe2ed21478f736dc78
Matija Pretnar <matija@pretnar.info>
2021-08-21 18:32:40 +0200

    Add trivial web backend model

--- src/05-backends/sig/web/webBackend.ml
+++ src/05-backends/sig/web/webBackend.ml
@@ -1,6 +1,12 @@
 module type S = sig
   include Backend.S
 
+  type msg
+  type model
+
+  val init : model
+  val update : model -> msg -> model
+  val view_model : model -> msg Vdom.vdom
   val view_step_label : step_label -> 'a Vdom.vdom
   val view_run_state : run_state -> step_label option -> 'a Vdom.vdom
 end
