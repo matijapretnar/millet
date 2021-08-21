10a0ea5c634df6cadd9c2838f3fb31f5021d4cfd
Matija Pretnar <matija@pretnar.info>
2021-08-21 19:00:21 +0200

    Let view_run_state depend on the backend model

--- src/05-backends/sig/web/webBackend.ml
+++ src/05-backends/sig/web/webBackend.ml
@@ -8,5 +8,5 @@ module type S = sig
   val update : model -> msg -> model
   val view_model : model -> msg Vdom.vdom
   val view_step_label : step_label -> 'a Vdom.vdom
-  val view_run_state : run_state -> step_label option -> 'a Vdom.vdom
+  val view_run_state : model -> run_state -> step_label option -> 'a Vdom.vdom
 end
